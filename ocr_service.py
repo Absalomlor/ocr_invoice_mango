f8ca52936a826a92c843dd63c5e96fb5a6288399
kung <krasirit.miao@gmail.com>
2025-07-08 15:49:40 +0700

    add api

--- ocr_service.py
+++ ocr_service.py
@@ -8,9 +8,13 @@ import re
 import pandas as pd
 import ast
 import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # --- CONFIG ---
-GEMINI_API_KEY = 'YOUR_GEMINI_API_KEY'
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_API_KEY}"
 HEADERS = {'Content-Type': 'application/json'}
 
